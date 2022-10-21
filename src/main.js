0dde486581397fceb0483f8196d292bd0794132e
Laura <lauramoerk@hotmail.com>
2022-10-21 22:17:15 +0200

    parrallax

--- src/main.js
+++ src/main.js
@@ -2,12 +2,3 @@ import "./style.css";
 
 import { animate, stagger, inView, scroll, timeline } from "motion";
 
-
-
-document.querySelectorAll("section").forEach((section) => {
-    const header = section.querySelector(".p");
-    scroll(animate(header, { y: [-100, 250] }), {
-      target: header
-    });
-  });
-
