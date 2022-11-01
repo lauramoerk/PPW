d9cfca1755f0addfe939eafe1e45f571d1745caa
Laura <lauramoerk@hotmail.com>
2022-11-01 19:34:42 +0100

    all pages added

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        graphics: resolve(__dirname, "src/graphics.html"),
+        illustrations: resolve(__dirname, "src/illustrations.html"),
+        websites: resolve(__dirname, "src/websites.html"),
         // about: resolve(__dirname, "src/about.html"),
       },
     },
